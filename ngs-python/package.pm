d37e4006c53ac79bff095aea396078247793d51f
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2014-10-17 18:30:42 -0400

    1350: ngs-python requires ngs-java and ncbi-vdb

--- ngs-python/package.pm
+++ ngs-python/package.pm
@@ -11,5 +11,32 @@ sub PKG { ( LNG  => 'PYTHON',
             PATH  => '/usr/local/ngs/ngs-python',
             UPATH =>      '$HOME/ngs/ngs-python',
             EXAMP => '1', ) }
-sub REQ {}
+sub REQ { (
+            { name    => 'ngs-sdk',
+              namew   => 'NGS',
+              option  => 'with-ngs-sdk-prefix',
+              origin  => 'I',
+              type    => 'L',
+              srcpath => '../ngs-sdk',
+              pkgpath => '/usr/local/ngs/ngs-sdk',
+              usrpath =>      '$HOME/ngs/ngs-sdk',
+              bldpath => '$HOME/ncbi-outdir/ngs-sdk',
+              include => 'ngs/itf/Refcount.h',
+              lib     => 'libngs-sdk.so',
+              ilib    => 'libngs-bind-c++.a',
+            },
+            { name    => 'ncbi-vdb',
+              namew   => 'VDB',
+              option  => 'with-ncbi-vdb-prefix',
+              origin  => 'I',
+              type    => 'L',
+              srcpath => '../../ncbi-vdb',
+              pkgpath => '/usr/local/ncbi/ncbi-vdb',
+              usrpath =>      '$HOME/ncbi/ncbi-vdb',
+              bldpath => '$HOME/ncbi-outdir/ncbi-vdb',
+              include => 'klib/rc.h',
+              lib     => 'libncbi-vdb.so',
+              ilib    => 'libkapp.a',
+             } ) }
+
 1
