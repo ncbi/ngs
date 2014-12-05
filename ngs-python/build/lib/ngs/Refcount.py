5452784319b58b5054f2f4aea7a3829078e46213
Andrew Klymenko <klymenka@mail.nih.gov>
2014-12-05 16:31:42 -0500

    1314: allow to have default build directory on ngs/ncbi-vdb/sra-tools level

---
+++ ngs-python/build/lib/ngs/Refcount.py
@@ -0,0 +1,65 @@
+from ctypes import byref, c_void_p
+
+from . import NGS
+from .String import NGS_RawString
+
+def RefcountRelease(ref):
+    """Releases NGS-object imported from ngs-sdk
+    
+    :param ref: reference to refcounted NGS-object to be released. It's expected to be of type c_void_p
+    :returns: None
+    :throws: ErrorMsg
+    """
+    ngs_str_err = NGS_RawString()
+    try:
+        res = NGS.lib_manager.PY_NGS_RefcountRelease(ref, byref(ngs_str_err.ref))
+    finally:
+        ngs_str_err.close()
+
+def RefcountRawStringRelease(ref):
+    """Releases raw string imported from ngs-sdk
+    
+    :param ref: reference to raw char string. It's expected to be of type c_char_p
+    :returns: None
+    :throws: ErrorMsg
+    """
+    ngs_str_err = NGS_RawString()
+    try:
+        res = NGS.lib_manager.PY_NGS_RawStringRelease(ref, byref(ngs_str_err.ref))
+    finally:
+        ngs_str_err.close()
+
+# def RefcountEngineRelease(ref):
+    # """Releases NGS-object imported from ngs engine
+    
+    # :param ref: reference to refcounted NGS-object to be released. It's expected to be of type c_void_p
+    # :returns: None
+    # :throws: ErrorMsg
+    # """
+    # with NGSEngine_String() as ngs_str_err:
+        # res = NGS.lib_manager.PY_NGS_Engine_RefcountRelease(ref, byref(ngs_str_err.ref))
+        # check_res(res, ngs_str_err)
+
+
+class Refcount:
+    """ Base class for all refcounted objects imported from ngs-sdk
+    """
+    def __init__(self):
+        self.init_members_with_null()
+    
+    def __del__(self):
+        self.close()
+            
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, t, value, traceback):
+        self.close()
+
+    def close(self):
+        if self.ref:
+            RefcountRelease(self.ref)
+            self.init_members_with_null()
+            
+    def init_members_with_null(self):
+        self.ref = c_void_p()
---
+++ ngs-python/build/lib/ngs/__init__.py
@@ -0,0 +1,43 @@
+# ===========================================================================
+# 
+#                            PUBLIC DOMAIN NOTICE
+#               National Center for Biotechnology Information
+# 
+#  This software/database is a "United States Government Work" under the
+#  terms of the United States Copyright Act.  It was written as part of
+#  the author's official duties as a United States Government employee and
+#  thus cannot be copyrighted.  This software/database is freely available
+#  to the public for use. The National Library of Medicine and the U.S.
+#  Government have not placed any restriction on its use or reproduction.
+# 
+#  Although all reasonable efforts have been taken to ensure the accuracy
+#  and reliability of the software and data, the NLM and the U.S.
+#  Government do not and cannot warrant the performance or results that
+#  may be obtained by using this software or data. The NLM and the U.S.
+#  Government disclaim all warranties, express or implied, including
+#  warranties of performance, merchantability or fitness for any particular
+#  purpose.
+# 
+#  Please cite the author in any work or product based on this material.
+# 
+# ===========================================================================
+# 
+# 
+
+#import os
+
+from .LibManager import LibManager # TODO probably, LibManager should not be the part of ngs package and be specific to engine
+
+PY_RES_OK    = 0
+
+class NGS:
+    lib_manager = LibManager()
+
+    @staticmethod
+    def openReadCollection(spec):
+        NGS.lib_manager.initialize_ngs_bindings()
+    
+        from .ReadCollection import openReadCollection  # entry point - adding name to ngs package global namespace
+        return openReadCollection(spec)
+
+
