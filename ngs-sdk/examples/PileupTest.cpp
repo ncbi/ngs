b9ef4891ca0cc3aaba9da3a5072d67dfbcb9d43b
yaschenk <yaschenk@ncbi.nlm.nih.gov>
2015-01-30 15:36:00 -0500

    adding PileupTest

---
+++ ngs-sdk/examples/PileupTest.cpp
@@ -0,0 +1,96 @@
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+*/
+
+#include <ncbi-vdb/NGS.hpp>
+#include <ngs/ErrorMsg.hpp>
+#include <ngs/ReadCollection.hpp>
+#include <ngs/Reference.hpp>
+#include <ngs/AlignmentIterator.hpp>
+#include <ngs/Alignment.hpp>
+
+#include <math.h>
+#include <iostream>
+
+using namespace ngs;
+using namespace std;
+
+class PileupTest
+{
+public:
+
+    static void run ( String acc, String refname, int start, int stop )
+    {
+
+        // open requested accession using SRA implementation of the API
+        ReadCollection run = ncbi::NGS::openReadCollection ( acc );
+        String run_name = run.getName ();
+
+        // get requested reference
+        Reference ref = run.getReference ( refname );
+
+        // start iterator on requested range
+        long count = stop - start + 1;
+        PileupIterator it = ref.getPileupSlice ( start, count);
+
+        long i;
+        for ( i = 0; it . nextPileup (); ++ i )
+        {
+            cout         << it.getReferenceSpec ()
+                 << '\t' << it.getReferencePosition ()
+                 << '\t' << it.getPileupDepth ()
+                 << '\n';
+        }
+
+        cerr << "Read " <<  i <<  " pileups for " <<  run_name << '\n';
+    }
+};
+
+int main ( int argc, char const *argv[] )
+{
+    if ( argc != 5 )
+    {
+        cerr << "Usage: PileupTest accession reference start stop\n";
+    }
+    else try
+    {
+        PileupTest::run ( argv[1], argv[2], atoi ( argv[3] ), atoi ( argv[4] ) );
+        return 0;
+    }
+    catch ( ErrorMsg & x )
+    {
+        cerr <<  x.toString () << '\n';
+    }
+    catch ( exception & x )
+    {
+        cerr <<  x.what () << '\n';
+    }
+    catch ( ... )
+    {
+        cerr <<  "unknown exception\n";
+    }
+
+    return 10;
+}
