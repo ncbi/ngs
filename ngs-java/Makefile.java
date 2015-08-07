3aaa1e814cb08fee92be8e008dceeeb978814460
klymenko <klymenka@mail.nih.gov>
2015-08-07 17:22:47 -0400

    VDB-1538: added Version to NCBI_SRC

--- ngs-java/Makefile.java
+++ ngs-java/Makefile.java
@@ -196,7 +196,8 @@ NCBI_SRC =                 \
 	LibManager             \
 	LibPathIterator        \
 	Manager                \
-	NGS
+	NGS                    \
+	Version                \
 
 NCBI_SRC_PATH = \
 	$(addprefix $(SRCDIR)/gov/nih/nlm/ncbi/ngs/,$(addsuffix .java,$(NCBI_SRC)))
