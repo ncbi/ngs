f90a6778179e1a04c90a6d27004161c591997ea0
aboshkin <boshkina@ncbi.nlm.nih.gov>
2015-09-01 10:05:47 -0400

    added get_filtered_alignments to Reference vtable

--- ngs-sdk/ngs/itf/ReferenceItf.h
+++ ngs-sdk/ngs/itf/ReferenceItf.h
@@ -92,6 +92,7 @@ struct NGS_Reference_v1_vt
     uint64_t ( CC * get_align_count ) ( const NGS_Reference_v1 * self, NGS_ErrBlock_v1 * err, bool wants_primary, bool wants_secondary );
     
     /* 1.3 interface */
+    struct NGS_Alignment_v1 * ( CC * get_filtered_alignments ) ( const NGS_Reference_v1 * self, NGS_ErrBlock_v1 * err, uint32_t flags, int32_t map_qual );
     struct NGS_Alignment_v1 * ( CC * get_filtered_align_slice ) ( const NGS_Reference_v1 * self, NGS_ErrBlock_v1 * err, int64_t start, uint64_t length, uint32_t flags, int32_t map_qual );
 };
 
